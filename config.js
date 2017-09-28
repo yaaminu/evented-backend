24b0f568615c381e006e80825da16a2a592410b5
yaaminu <afolanyaaminu@gmail.com>
2017-09-28 17:57:22 +0000

    update app id

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/evented'
 exports.MASTER_KEY = process.env.MASTER_KEY ||
     'p+PKpCjHQMXPW3X+Bja47GP34g38YH9uXc0t85kFwxBVzFlHZPgrh/XrYlleJdE1DIqvd0mQzL1vZuxTdfxjwor2Lgz1Rz5uan7f8y2alxk+NPWM2Q5gkqOoBkWu2O2bQEN8moTI2qL+WR3h1BbGo/8hKE9pDpfP34wppCrgBlM='
 exports.APP_ID = process.env.APP_ID ||
-    '8rRIZo9dfTmTQ76ZZ8WHmoNejWiAEMJ7y7Bq6531ZpOD5mR9NSXrAm9mBF2lb76O'
+    '8rRIfyacacare092kacabcadw.lkwZo9dfTmTQ76ZZ8WHmoNejWiAEMJ7y7Bq6531ZpOD5mR9NSXrAm9mBF2lb76O'
 exports.PORT = process.env.PORT || 8080
 exports.CLOUD_CODE_PATH = require('path').resolve(__dirname, 'cloud/main.js')
 exports.SERVER_URL = process.env.SERVER_URL || 'http://localhost:8080/'
